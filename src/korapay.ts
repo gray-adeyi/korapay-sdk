4f7f86ea6937042524c7f1be2544d2ecabf1c7a2
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-02 17:06:48 +0100

    Implement KorapayClient

--- src/korapay.ts
+++ src/korapay.ts
@@ -1,6 +1,17 @@
-import RestClient, { HTTPMethod } from "./restClient.ts";
+import { HTTPMethod, type Country, type Currency } from "./enums.ts";
+import RestClient from "./restClient.ts";
+import type {
+  AuthorizeCardChargePayload,
+  ChargeViaBankTransferPayload,
+  ChargeViaCardPayload,
+  ChargeViaMobileMoneyPayload,
+  CreateVirtualBankAccountPayload,
+  InitiateChargePayload,
+  PayoutToBankAccountPayload,
+  PayoutToMobileMoneyPayload,
+} from "./types/global.ts";
 
-export default class KoraPayClient {
+export default class KorapayClient {
   client: RestClient;
   constructor(publicKey?: string, secretKey?: string, client?: RestClient) {
     if (client) {
@@ -10,77 +21,197 @@ export default class KoraPayClient {
     }
   }
 
-  chargeViaCard(){
-    // 
+  async chargeViaCard(payload: ChargeViaCardPayload) {
+    const chargeData = await this.client.encryptData(payload);
+    return await this.client.call(
+      "/merchant/api/v1/charges/card",
+      HTTPMethod.POST,
+      { chargeData },
+    );
   }
 
-  authorizeCardCharge(){
-    // 
+  authorizeCardCharge(payload: AuthorizeCardChargePayload) {
+    return this.client.call(
+      "/merchant/api/v1/charges/card/authorize",
+      HTTPMethod.POST,
+      payload,
+    );
   }
 
-  resendCardOtp(){
-    // 
+  resendCardOtp(transactionReference: string) {
+    return this.client.call(
+      "/merchant/api/v1/charges/card/resend-otp",
+      HTTPMethod.POST,
+      { transactionReference },
+    );
   }
 
-  chargeViaBankTransfer(){
-    // 
+  chargeViaBankTransfer(payload: ChargeViaBankTransferPayload) {
+    return this.client.call(
+      "/merchant/api/v1/charges/bank-transfer",
+      HTTPMethod.POST,
+      payload,
+    );
   }
 
-  createVirtualBankAccount(){
-    // 
+  createVirtualBankAccount(payload: CreateVirtualBankAccountPayload) {
+    return this.client.call(
+      "/merchant/api/v1/virtual-bank-account",
+      HTTPMethod.POST,
+      payload,
+    );
   }
 
-  getVirtualBankAccount(){
-    // 
+  getVirtualBankAccount(accountReference: string) {
+    return this.client.call(
+      `/merchant/api/v1/virtual-bank-account/${accountReference}`,
+      HTTPMethod.GET,
+    );
   }
 
-  getVirtualBankAccountTransactions(){}
+  getVirtualBankAccountTransactions(accountNumber: string) {
+    return this.client.call(
+      `/merchant/api/v1/virtual-bank-account/transactions?account_number=${accountNumber}`,
+      HTTPMethod.GET,
+    );
+  }
 
-  creditSandboxVirtualBankAccount(){}
+  creditSandboxVirtualBankAccount(
+    accountNumber: string,
+    amount: number,
+    currency: Currency,
+  ) {
+    return this.client.call(
+      "/merchant/api/v1/virtual-bank-account/sandbox/credit",
+      HTTPMethod.POST,
+      { accountNumber, amount, currency },
+    );
+  }
 
-  chargeViaMobileMoney(){}
+  chargeViaMobileMoney(payload: ChargeViaMobileMoneyPayload) {
+    return this.client.call(
+      "/merchant/api/v1/charges/mobile-money",
+      HTTPMethod.POST,
+      payload,
+    );
+  }
 
-  authorizeMobileMoneyCharge(){}
+  authorizeMobileMoneyCharge(reference: string, token: string) {
+    return this.client.call(
+      "/merchant/api/v1/charges/mobile-money/authorize",
+      HTTPMethod.POST,
+      { reference, token },
+    );
+  }
 
-  resendMobileMoneyOtp(){}
+  resendMobileMoneyOtp(transactionReference: string) {
+    return this.client.call(
+      "/merchant/api/v1/charges/mobile-money/resend-otp",
+      HTTPMethod.POST,
+      { transactionReference },
+    );
+  }
 
-  resendSkt(){}
+  resendSkt(transactionReference: string) {
+    return this.client.call(
+      "/merchant/api/v1/charges/mobile-money/resend-stk",
+      HTTPMethod.POST,
+      { transactionReference },
+    );
+  }
 
-  authorizeSkt(){}
+  authorizeSkt(reference: string, pin: string) {
+    return this.client.call(
+      "/merchant/api/v1/charges/mobile-money/sandbox/authorize-stk",
+      HTTPMethod.POST,
+      { reference, pin },
+    );
+  }
 
-  initiateCharge(){}
+  initiateCharge(payload: InitiateChargePayload) {
+    return this.client.call(
+      "/merchant/api/v1/charges/initialize",
+      HTTPMethod.POST,
+      payload,
+    );
+  }
 
-  getCharge(){}
+  getCharge(reference: string) {
+    return this.client.call(
+      `/merchant/api/v1/charges/${reference}`,
+      HTTPMethod.GET,
+    );
+  }
 
-  resolveBankAccount(){}
+  resolveBankAccount(bankCode: string, accountNumber: string) {
+    return this.client.call(
+      "/merchant/api/v1/misc/banks/resolve",
+      HTTPMethod.POST,
+      { bank: bankCode, account: accountNumber },
+    );
+  }
 
   getBalances() {
     return this.client.call("/merchant/api/v1/balances", HTTPMethod.GET);
   }
 
-  getBanks() {
-    //
+  getBanks(country: Country) {
+    return this.client.call(
+      `/merchant/api/v1/misc/banks?countryCode=${country}`,
+      HTTPMethod.GET,
+      true
+    );
   }
 
-  getMmo() {
-    //
+  getMmo(country: Country) {
+    return this.client.call(
+      `/merchant/api/v1/misc/mobile-money?countryCode=${country}`,
+      HTTPMethod.GET,
+    );
   }
 
-  payoutToBankAccount(){}
-
-  payoutToMobileMoney(){}
-
-  bulkPayoutToBankAccount(){}
-
-  getPayouts(bulkReference: string){
-    return this.client.call(`/api/v1/transactions/bulk/${bulkReference}/payout`, HTTPMethod.GET)
+  payoutToBankAccount(payload: PayoutToBankAccountPayload) {
+    return this.client.call(
+      "/merchant/api/v1/transactions/disburse",
+      HTTPMethod.POST,
+      payload,
+    );
   }
 
-  getBulkTransaction(bulkReference: string){
-    return this.client.call(`/api/v1/transactions/bulk/${bulkReference}`, HTTPMethod.GET)
+  payoutToMobileMoney(payload: PayoutToMobileMoneyPayload) {
+    return this.client.call(
+      "/merchant/api/v1/transactions/disburse",
+      HTTPMethod.POST,
+      payload,
+    );
   }
 
-  getPayoutTransaction(transactionReference: string){
-    return this.client.call(`/merchant/api/v1/transactions/${transactionReference}`, HTTPMethod.GET)
+  bulkPayoutToBankAccount(payload: PayoutToBankAccountPayload) {
+    return this.client.call(
+      "/api/v1/transactions/disburse/bulk",
+      HTTPMethod.POST,
+      payload,
+    );
+  }
+
+  getPayouts(bulkReference: string) {
+    return this.client.call(
+      `/api/v1/transactions/bulk/${bulkReference}/payout`,
+      HTTPMethod.GET,
+    );
+  }
+
+  getBulkTransaction(bulkReference: string) {
+    return this.client.call(
+      `/api/v1/transactions/bulk/${bulkReference}`,
+      HTTPMethod.GET,
+    );
+  }
+
+  getPayoutTransaction(transactionReference: string) {
+    return this.client.call(
+      `/merchant/api/v1/transactions/${transactionReference}`,
+      HTTPMethod.GET,
+    );
   }
 }
