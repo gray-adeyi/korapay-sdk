dec033e10f6777798341cd2cf376904db5bddb1f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-09-02 17:06:16 +0100

    Refactor RestClient

--- src/restClient.ts
+++ src/restClient.ts
@@ -1,4 +1,4 @@
-import { KoraPayClientError } from "./errors.ts";
+import { KorapayClientError } from "./errors.ts";
 import axios from "axios";
 import type {
   AxiosError,
@@ -8,20 +8,14 @@ import type {
 } from "axios";
 import { camelCase, snakeCase } from "lodash-es";
 import { encryptAes256 } from "./utils.ts";
-
-export enum HTTPMethod {
-  GET,
-  POST,
-  PATCH,
-  PUT,
-  DELETE,
-}
+import type { KorapayResponse } from "./types/global.ts";
+import { HTTPMethod } from "./enums.ts";
 
 export default class RestClient {
   static BASE_URL = "https://api.korapay.com";
   static ENV_PUBLIC_KEY_NAME = "KORAPAY_PUBLIC_KEY";
   static ENV_SECRET_KEY_NAME = "KORAPAY_SECRET_KEY";
-  static ENV_ENCRYPTION_KEY_NAME = "KORAPAY_ENCRYPTION_KEY"
+  static ENV_ENCRYPTION_KEY_NAME = "KORAPAY_ENCRYPTION_KEY";
 
   private publicKey!: string;
   private secretKey!: string;
@@ -46,29 +40,36 @@ export default class RestClient {
     );
   }
 
-  
-
   // deno-lint-ignore no-explicit-any
-  async call(endpoint: string, method: HTTPMethod, data?: any) {
+  async call(endpoint: string, method: HTTPMethod, data?: any, noAuth = false) {
     const handler = this.getMethodHandler(method);
     let response: AxiosResponse;
     if ([HTTPMethod.GET, HTTPMethod.DELETE].includes(method)) {
-      response = await handler(endpoint);
+      response = noAuth ? await handler(endpoint, {headers: {Authorization: ''}}) : await handler(endpoint);
     } else {
-      response = await handler(endpoint, data);
+      response =noAuth ? await handler(endpoint,data,{headers:{Authorization:''}}) : await handler(endpoint, data);
     }
-    return response;
+    return this.deserializeResponse(response);
   }
 
-  encryptData(data: any){
-    return encryptAes256(this.encryptionKey, data)
+  // deno-lint-ignore no-explicit-any
+  encryptData(data: any) {
+    return encryptAes256(this.encryptionKey, data);
   }
 
-private get headers() {
+  private get baseHeaders(){
     return {
-      Authorization: `Bearer ${this.secretKey}`,
-      "User-Agent": "@gray-adeyi/korapay-sdk 0.1.0",
+"User-Agent": "@gray-adeyi/korapay-sdk 0.1.0",
       Accept: "application/json",
+      'Content-Type': 'application/json',
+      Authorization: ''
+    }
+  }
+
+  private get headers() {
+    return {
+      ...this.baseHeaders,
+      Authorization: `Bearer ${this.secretKey}`,
     };
   }
 
@@ -87,15 +88,24 @@ private get headers() {
     }
   }
 
+  private deserializeResponse(response: AxiosResponse): KorapayResponse {
+    return {
+      statusCode: response.status,
+      status: response.data["status"] || false,
+      message: response.data["message"],
+      data: response.data["data"],
+    };
+  }
+
   private requestPayloadTransformerInterceptor(
     config: InternalAxiosRequestConfig,
   ) {
     config.data = RestClient.camelToSnakeCaseTransformer(config.data);
+    console.log(config)
     return config;
   }
 
   private responsePayloadTransformerInterceptor(response: AxiosResponse) {
-    console.log("before treansform", response.data);
     response.data = RestClient.snakeToCamelCaseTransformer(response.data);
     return response;
   }
@@ -105,6 +115,7 @@ private get headers() {
     if (Array.isArray(data)) {
       return data.map(this.camelToSnakeCaseTransformer);
     } else if (data !== null && typeof data === "object") {
+      // deno-lint-ignore no-explicit-any
       return Object.keys(data).reduce((acc: Record<string, any>, key) => {
         const snakeKey: string = snakeCase(key);
         acc[snakeKey] = this.camelToSnakeCaseTransformer(data[key]);
@@ -119,6 +130,7 @@ private get headers() {
     if (Array.isArray(data)) {
       return data.map(this.snakeToCamelCaseTransformer);
     } else if (data !== null && typeof data === "object") {
+      // deno-lint-ignore no-explicit-any
       return Object.keys(data).reduce((acc: Record<string, any>, key) => {
         const camelKey = camelCase(key);
         acc[camelKey] = this.snakeToCamelCaseTransformer(data[key]);
@@ -130,13 +142,13 @@ private get headers() {
 
   private handleRequestError(error: AxiosError) {
     return Promise.reject(
-      new KoraPayClientError(error.message, error.status, error.code),
+      new KorapayClientError(error.message, error.status, error.code),
     );
   }
 
   private handleResponseError(error: AxiosError) {
     return Promise.reject(
-      new KoraPayClientError(error.message, error.status, error.code),
+      new KorapayClientError(error.message, error.status, error.code, error),
     );
   }
 
@@ -146,7 +158,7 @@ private get headers() {
       return;
     }
     if (!Deno.env.has(RestClient.ENV_PUBLIC_KEY_NAME)) {
-      throw new KoraPayClientError(
+      throw new KorapayClientError(
         `public key was not provided on instantiation or set in environmental variables as ${RestClient.ENV_PUBLIC_KEY_NAME}`,
       );
     }
@@ -159,23 +171,25 @@ private get headers() {
       return;
     }
     if (!Deno.env.has(RestClient.ENV_SECRET_KEY_NAME)) {
-      throw new KoraPayClientError(
+      throw new KorapayClientError(
         `secret key was not provided on instantiation or set in environmental variables as ${RestClient.ENV_SECRET_KEY_NAME}`,
       );
     }
     this.secretKey = Deno.env.get(RestClient.ENV_SECRET_KEY_NAME) as string;
   }
 
-private loadEncryptionKey(value?: string) {
+  private loadEncryptionKey(value?: string) {
     if (value) {
       this.encryptionKey = value;
       return;
     }
     if (!Deno.env.has(RestClient.ENV_ENCRYPTION_KEY_NAME)) {
-      throw new KoraPayClientError(
+      throw new KorapayClientError(
         `encryption key was not provided on instantiation or set in environmental variables as ${RestClient.ENV_ENCRYPTION_KEY_NAME}`,
       );
     }
-    this.encryptionKey = Deno.env.get(RestClient.ENV_ENCRYPTION_KEY_NAME) as string;
+    this.encryptionKey = Deno.env.get(
+      RestClient.ENV_ENCRYPTION_KEY_NAME,
+    ) as string;
   }
 }
